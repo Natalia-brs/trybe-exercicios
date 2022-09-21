a71c870e6790f3b66af4742b5904e8cc6469edae
natalia <ntla.brs@gmail.com>
2022-09-21 14:54:04 -0300

    Variaveis com const e let

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-1-javascript-primeiros-passos/variaveis.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-1-javascript-primeiros-passos/variaveis.js
@@ -2,3 +2,10 @@ let myName = 'Natalia';
 let birthCity = 'Piracicaba/SP';
 let birthYear = 1994;
 
+console.log (myName, birthCity, birthYear);
+
+const meuNome = 'Natalia';
+const cidadeNasc = 'Piracicaba/SP';
+const anoNasc = 1994;
+
+console.log(meuNome, cidadeNasc, anoNasc);
