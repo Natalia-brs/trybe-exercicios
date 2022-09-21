91ddddc07e29fd3b7b29c6451a88cffa63d79350
natalia <ntla.brs@gmail.com>
2022-09-21 18:57:45 -0300

    Resolução exercicio 5

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-1-javascript-primeiros-passos/rocket.js
@@ -0,0 +1,23 @@
+ 
+ // Exercicio 5 - Ângulos internos
+ let a = 35;
+ let b = 75;
+ let c = 70;
+
+ let soma = a + b + c;
+
+ if (a < 0  || b < 0 || c < 0) {
+    console.log ('Valor invalido');
+ }
+ else if (soma == 180) {
+    console.log (true);
+ }
+ else {
+    console.log (false);
+ }
+
+ 
+    
+
+ 
+ 
