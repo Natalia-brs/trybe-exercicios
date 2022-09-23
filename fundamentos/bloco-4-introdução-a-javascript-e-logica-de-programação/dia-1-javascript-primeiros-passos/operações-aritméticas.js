b34617be95f33e43fea1c62987162cc7d6f35935
natalia <ntla.brs@gmail.com>
2022-09-22 21:39:42 -0300

    Exercicio currentHour

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-1-javascript-primeiros-passos/operações-aritméticas.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-1-javascript-primeiros-passos/operações-aritméticas.js
@@ -1,7 +1,31 @@
-const base = 5;
-let height = 8;
+// const base = 5;
+// let height = 8;
 
-const area = base * height;
-const perimeter = area + base + height;
+// const area = base * height;
+// const perimeter = area + base + height;
 
-console.log (area, perimeter);
+// console.log (area, perimeter);
+
+ const currentHour = 15.00;
+ let message = '';
+  
+ if (currentHour >= 22) {
+   message = 'Não deveriamos comer nada, é hora de dormir'
+   console.log (message);
+ }
+ else if (currentHour >= 18  && currentHour < 22) {
+    message = 'Rango da noite, vamos jantar :D'
+    console.log (message);
+ }
+ else if (currentHour >= 14 && currentHour < 18) {
+    message = 'Vamos fazer um bolo pro café da tarde?'
+    console.log (message);
+ }
+ else if (currentHour >= 11 && currentHour < 14 ) {
+    message = 'Hora do almoço!'
+    console.log (message);
+ }
+ else {
+    message = 'Hmm, cheiro de café recém-passado'
+    console.log (message);
+ }
