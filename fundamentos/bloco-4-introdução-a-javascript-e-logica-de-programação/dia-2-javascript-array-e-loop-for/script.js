d94a2da0bbf10258393057cc099bac8530ecab3b
natalia <ntla.brs@gmail.com>
2022-09-23 12:19:15 -0300

    organizando o codigo

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-2-javascript-array-e-loop-for/script.js
@@ -1,63 +1,85 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+ // ---------------- imprimindo todos os valores do array --------------//
+ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
  for (let index = 0; index < numbers.length; index += 1 ) {
      console.log (numbers [index]);
 }
+// -------------------------------------------------------------------- //
 
+
+// ------ somando todos os valores contidos no array e exibindo  ------- //
   let soma = 0;
  for (let index = 0; index < numbers.length; index +=1) {
     soma = soma + numbers[index];
 }
   console.log (soma);
+// ------------------------------------------------------------------------//
 
+
+ // ------ fazendo a media dos elementos do array -----------//
  let media = soma/numbers.length;
  console.log (media);
+// -----------------------------------------------------------//
 
+ 
+ //------ Exibindo se o valor e maior ou igual a 20 ----------//
   if (media > 20) {
      console.log ('Valor maior que 20')
  } 
  else {
     console.log ('Valor menor ou igual a 20')
 }
+// ----------------------------------------------------------//
 
-//  let maior = 0;
-// for (let index = 0; index < numbers.length; index += 1) {
-//     if (numbers[index] > maior) {
-//         maior = numbers[index]
-//     }
-// }
-//  console.log (maior)
 
-// let contimpares =0;
-// for (let index = 0; index <numbers.length; index += 1) {
-//      if (numbers[index] % 2 !== 0 ) {
-//             contimpares += 1
-//      }
-// }
+ // ------- exibir o maior valor dentro do array -----------//
+  let maior = 0;
+  for (let index = 0; index < numbers.length; index += 1) {
+      if (numbers[index] > maior) {
+          maior = numbers[index]
+       }
+   }
+   console.log (maior)
+//--------------------------------------------------------------//
 
-// if (contimpares == 0) {
-//     console.log ('Nenhum valor encontrado')
-// }
-// else {
-//     console.log (contimpares)
-// }
 
-// let menor = numbers[0]
-// for (let index = 1; index < numbers.length; index += 1) {
-//     if (numbers[index] < menor) {
-//         menor = numbers[index]
-//   }
-// }
-// console.log (menor);
+//----------- valores impares do array -----------------------//
+   let contimpares =0;
+   for (let index = 0; index < numbers.length; index += 1) {
+       if (numbers[index] % 2 !== 0 ) {
+              contimpares += 1
+       }
+  }
+  if (contimpares == 0) {
+     console.log ('Nenhum valor encontrado')
+ }
+ else {
+     console.log (contimpares)
+ }
+ // -----------------------------------------------------------------//
+ 
 
-// let array = [];
+ // ----------- menor valor contido no array ------------------------ //
+ let menor = numbers[0]
+  for (let index = 1; index < numbers.length; index += 1) {
+      if (numbers[index] < menor) {
+          menor = numbers[index]
+    }
+  }
+  console.log (menor);
+// -------------------------------------------------------------------//
 
-// for (let index = 1; index <= 25; index += 1) {
-//   array.push(index);
-// }
 
-// console.log(array);
+ //-- criando array de 1 a 25 e fazendo a divisão de cada elemento por 2 --//
+  let array = [];
 
-// for (let index = 0; index < array.length; index += 1) {
-//     console.log(array[index] / 2);
-//   };
+  for (let index = 1; index <= 25; index += 1) {
+    array.push(index);
+   }
+
+   console.log(array);
+
+  for (let index = 0; index < array.length; index += 1) {
+      console.log(array[index] / 2);
+    };
+// ---------------------------------------------------------------- //
