3b9d7ab2d94b91cda199c7befc425c29f26a6c65
natalia <ntla.brs@gmail.com>
2022-09-26 18:58:28 -0300

    Organizando os exercicios

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-4-javascript-objetos-e-funções/script.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-4-javascript-objetos-e-funções/script.js
@@ -1,15 +1,71 @@
 
-let player = {
-    name: 'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals: {
-        golden: 2,
-        silver:3,
-    },
-};
 
-console.log (player.name + ' ' + player.lastName); // pode se acessar os elementos utlizando .
-console.log (player ['name'] + " " + player['lastName']) // pou pode acessar os elementos utilizando []
-console.log (player.medals.golden)
-console.log (player)
+// ---------Montando um objeto ---------------//
+
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: {
+//         golden: 2,
+//         silver:3,
+//     },
+// };
+
+// console.log (player.name + ' ' + player.lastName); // pode se acessar os elementos utlizando .
+// console.log (player ['name'] + " " + player['lastName']) // pou pode acessar os elementos utilizando []
+// console.log (player.medals.golden)
+// console.log (player)
+
+//------------------------------------------------------------------------------------------------------------//
+
+
+//-----------Selecionando uma chave especifica ------------------//
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log ('Bem-vinda,' + ' ' + info.personagem)
+
+//-----------------------------------------------------------------//
+
+
+//------------Inserindo uma nova propriedade com brackets notation----------//
+
+  info['recorrente'] = 'Sim';
+  console.log (info['recorrente'])
+
+//--------------------------------------------------------------------------//  
+
+//----------------For/in exibindo apenas as chaves do objeto ---------------//
+
+  for (let key in info) {
+    console.log (key)
+  }
+
+//--------------------------------------------------------------------------//
+
+//-------------- for/in exibindo apenas os valores das chaves --------------//
+
+  for (let key in info) {
+    console.log (info[key])
+  }
+
+//--------------------------------------------------------------------------//
+
+//------------ Novo objeto criado e exibindo os valores dos dois objetos juntos ----------//
+
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'MacPatinhas',
+    recorrente: 'Sim',
+  };
+
+  for (let index in info) {
+    console.log (info[index] + ' ' + 'e' + ' ' + info2[index])
+  }
+
+  //--------------------------------------------------------------------------------------------//
