1ab07bd3d325fc1e73922a6665edb926874e71ab
natalia <ntla.brs@gmail.com>
2022-09-26 15:37:24 -0300

    Criando objetos

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-4-javascript-objetos-e-funções/objetos.js
@@ -0,0 +1,12 @@
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver:3,
+    },
+};
+
+console.log (player.name + ' ' + player.lastName);
