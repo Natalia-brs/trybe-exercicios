6a669ca7a557b0050c1bf50bad9ad88638062b1d
natalia <ntla.brs@gmail.com>
2022-09-26 21:55:01 -0300

    exercicio maior qnt de caracteres

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-4-javascript-objetos-e-funções/funçoes.js
@@ -0,0 +1,58 @@
+//===========================Exercicios Funções====================================================//
+
+//---------------Verificando se é um palindromo ------------------------------------//
+
+
+// let verifyPalindromo = '';
+// function palindromo (palavra) {
+//   verifyPalindromo = palavra.split('').reverse().join('');
+//       if (verifyPalindromo === palavra ) {
+//           verifyPalindromo = true;  
+//     } 
+//     else {
+//          verifyPalindromo = false;
+//     }
+// return verifyPalindromo;
+// }
+
+// console.log (palindromo('arara'));
+
+//-------------------------------------------------------------------------------//
+
+//--------------Função que retorna o indice do maior valor de um array-----------------------//
+
+
+// function maior (array) {
+//     let maiorIndice = 0;
+//     for (let index in array) {
+//         if (maiorIndice < array[index])
+//         maiorIndice = index
+//     }
+//     return maiorIndice;
+//    }
+// console.log (maior([5, 15, 45, 60, 80, 100]));
+ 
+
+//-------------------------------------------------------------------------------------------//
+
+
+//-------------Função que retorna o indice de menor valor------------------------------------//
+
+
+// function menor (array) {
+//     let indiceMenor = 0;
+//     for (let index in array) {
+//         if (array[index] < indiceMenor) {
+//             indiceMenor = index
+//         }
+//     }
+// return indiceMenor;
+// }
+// console.log (menor([5, 20, 15, 60, 80, -5]))
+
+
+//--------------------------------------------------------------------------------------------//
+
+
+//------------Função que retorna a string com maior numero de caracteres ---------------------//
+
