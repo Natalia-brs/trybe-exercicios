6e4ea0cac8e1173e2bb5108d127f8724a4cb81de
natalia <ntla.brs@gmail.com>
2022-09-28 15:51:14 -0300

    Manipulando seletores DOM - getElementsById

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,6 @@
+document.getElementById('page-title').innerText = 'Interestellar';
+document.getElementById('second-paragraph').style.color = 'blue'
+ document.getElementById('second-paragraph').style.fontFamily = 'monospace'
+document.getElementById('second-paragraph').style.fontSize = '25px'
+document.getElementById('paragraph').style.backgroundColor = 'lightblue'
+document.getElementById('subtitle').innerText = 'Melhor filme'
