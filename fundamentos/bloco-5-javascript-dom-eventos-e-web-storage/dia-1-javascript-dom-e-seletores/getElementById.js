181a70bee0cfffe89ca4f0fd3ee5a1ecdf7eb4ce
natalia <ntla.brs@gmail.com>
2022-09-28 19:09:22 -0300

    Alterando o texto da tag <p>

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/getElementById.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('page-title').innerText = 'Interestellar';
-document.getElementById('second-paragraph').style.color = 'blue'
- document.getElementById('second-paragraph').style.fontFamily = 'monospace'
-document.getElementById('second-paragraph').style.fontSize = '25px'
-document.getElementById('paragraph').style.backgroundColor = 'lightblue'
-document.getElementById('subtitle').innerText = 'Melhor filme'
