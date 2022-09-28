9b4c4624db05f4e6d584bfd8836a2e86679a08a2
natalia <ntla.brs@gmail.com>
2022-09-28 18:52:42 -0300

    Usando o getElementsByClassName

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/getElementsByClassName.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/getElementsByClassName.js
@@ -1,3 +1,3 @@
-document.getElementsByClassName('second-paragraph')[0].style.color = 'blue'
-
-
+document.getElementsByClassName('second-paragraph')[0].style.color = 'blue';
+document.getElementsByClassName('second-paragraph')[1].style.color = 'lightblue';
+document.getElementsByTagName ('h4')[0].style.color = 'orange'
