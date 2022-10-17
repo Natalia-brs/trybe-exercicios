b44c9f57306035d504fa40c65427f6c7edb4b42a
natalia <ntla.brs@gmail.com>
2022-10-17 20:45:54 -0300

    Alterando a estrutura da faunção para arrow function

---
+++ fundamentos/seção-7-introdução-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let,const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,19 @@
+
+//Exercicio 1:
+//Modifique a estrutura da função para que ela seja uma arrow function;
+//Modifique as variáveis para que respeitem o escopo onde estão declaradas;
+//Modifique as concatenações para template literals.
+
+const testingScope = (escopo) => {
+    if (escopo === true) {
+      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      var elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(true);
