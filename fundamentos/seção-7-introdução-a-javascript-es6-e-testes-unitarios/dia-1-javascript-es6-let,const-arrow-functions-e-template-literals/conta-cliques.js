0caaf3d89b4395cc8f2075e1a48637ec51dc42be
natalia <ntla.brs@gmail.com>
2022-10-17 21:38:35 -0300

    Exercicio 3 feito

---
+++ fundamentos/seção-7-introdução-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let,const-arrow-functions-e-template-literals/conta-cliques.js
@@ -0,0 +1,19 @@
+//----------------------Exercicio 3-------------------------------//
+
+//Crie uma variável clickCount que irá acumular o número de clicks do botão
+//Crie a lógica do evento, para que a cada click no botão, a variável seja atualizada com acréscimo de valor 1
+
+const tagCLick = document.getElementById('click');
+let button = document.getElementById('button');
+
+const clickCount = () => {
+ let click = 0;
+ button.addEventListener('click', () => {
+    button = click += 1;
+    tagCLick.innerHTML = button;
+ })
+
+}
+clickCount();
+
+//-------------------------------------------------------------------------//
