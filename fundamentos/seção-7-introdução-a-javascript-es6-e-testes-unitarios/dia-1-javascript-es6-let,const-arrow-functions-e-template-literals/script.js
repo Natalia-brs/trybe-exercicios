e518a82fc94892abe912bc6343887bf127392793
natalia <ntla.brs@gmail.com>
2022-10-17 21:24:25 -0300

    Exercicio 2

--- fundamentos/seção-7-introdução-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let,const-arrow-functions-e-template-literals/script.js
+++ fundamentos/seção-7-introdução-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let,const-arrow-functions-e-template-literals/script.js
@@ -9,10 +9,10 @@ const testingScope = (escopo) => {
     if (escopo === true) {
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
       ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
+      return ifScope;
     } else {
       const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
-      console.log(elseScope);
+      return elseScope;
     }
 
   }
@@ -20,3 +20,21 @@ const testingScope = (escopo) => {
   testingScope(true);
 
 //----------------------------------------------------------------------------------//
+
+//----------------------------------Exercicio 2-------------------------------------//
+
+//Crie uma função que faça com que o array oddsAndEvens fique em ordem crescente;
+//Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>)
+//retorne a frase “Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!”;
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = (a, b) => {
+  return (a - b)
+}
+
+oddsAndEvens.sort(sortOddsAndEvens)
+
+console.log(` Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+
+//-----------------------------------------------------------------------------------------//
