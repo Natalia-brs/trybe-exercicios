508524061d65e4eec7ec89381dd154b2650f5dce
natalia <ntla.brs@gmail.com>
2022-10-25 15:19:04 -0300

    segundo exercicio

--- fundamentos/seção-8-high-order-functions-do-javascript-es6/dia-2-forEach-find-some-every-sort/find.js
+++ fundamentos/seção-8-high-order-functions-do-javascript-es6/dia-2-forEach-find-some-every-sort/find.js
@@ -1,6 +1,9 @@
-// Utilize o find para retornar o primeiro numero do array que e divisivel por 5 caso ele exista
 
-// -------- forma 1-----------------------------------------------//
+
+// -------- exercicio 1 forma 1 -----------------------------------------------//
+// Utilize o find para retornar o primeiro numero do array que e divisivel por 5 caso ele exista.
+
+
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const findDivisibleBy3And5 = () => {
@@ -13,9 +16,8 @@ console.log(findDivisibleBy3And5());
 //exibindo o resultado no console.
 // Aqui nao tem a necessidade de passar um parametro para a função findDivisibleBy3And5, pois vc fará isso no find.
 
-//---------------------------------------------------------------------//
 
-//--------- forma 2 --------------------------------------------------//
+//--------- exercicio 1 forma 2 --------------------------------------------------//
 
 const anotherWay = (num) => num % 3 === 0 && num % 5 === 0;
 
@@ -26,5 +28,29 @@ console.log(receive);
 //Aqui fizemos o mesmo exercicio mas de forma diferente, criamos a função anotherWay com o parametro num, que irá fazer
 // a interação pelo array e comparar se os numeros são divisiveis;
 // Depois criamos uma variavel que ira guardar esse valor e passamos o find nela, recebendo como callback a função anotherWay.
+
+
+//---------------------------------------------------------------------------------//
+
+
+//-------------------- exercicio 2 ---------------------------------//
+//forma 1
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  return names.find((letter) => letter.length === 5);
+};
+
+console.log(findNameWithFiveLetters());
+
+//forma 2
+
+
+const anotherFunc = (letter) => letter.length === 5;
+
+ const result = names.find(anotherFunc);
+
+ console.log(result);
 
 //-------------------------------------------------------------------//
