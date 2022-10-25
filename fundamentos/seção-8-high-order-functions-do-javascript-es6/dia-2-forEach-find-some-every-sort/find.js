a20df41aa1a18c82e211e039f528058bb68243d3
natalia <ntla.brs@gmail.com>
2022-10-25 15:27:56 -0300

    exercicio 3

--- fundamentos/seção-8-high-order-functions-do-javascript-es6/dia-2-forEach-find-some-every-sort/find.js
+++ fundamentos/seção-8-high-order-functions-do-javascript-es6/dia-2-forEach-find-some-every-sort/find.js
@@ -54,3 +54,20 @@ const anotherFunc = (letter) => letter.length === 5;
  console.log(result);
 
 //-------------------------------------------------------------------//
+
+//--------------------- exercicio 3 ---------------------------------//
+
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ];
+  
+  function findMusic(id) {
+   return musicas.find((mus) => mus.id === id);
+  };
+  
+  console.log(findMusic('31031685'));
+
+
+//---------------------------------------------------------------//
