61a6de063051f35b94d13eb7eb35e7559335cd2c
natalia <ntla.brs@gmail.com>
2022-10-26 21:30:26 -0300

    Refazendo os exercicios da aula

---
+++ fundamentos/seção-8-high-order-functions-do-javascript-es6/dia-3-map-e-filter/exercise2.js
@@ -0,0 +1,35 @@
+const states = [
+    { short: "AM", name: "Amazonas" },
+    { short: "PA", name: "Pará" },
+    { short: "TO", name: "Tocantins" },
+    { short: "MG", name: "Minas Gerais" },
+    { short: "BA", name: "Bahia" },
+    { short: "PR", name: "Paraná" },
+    { short: "SP", name: "São Paulo" },
+    { short: "RN", name: "Rio Grande do Norte" },
+    { short: "CE", name: "Ceará" },
+    { short: "MT", name: "Mato Grosso" },
+  ];
+  
+  const regions = [
+    { short: "N", name: "Norte" },
+    { short: "NE", name: "Nordeste" },
+    { short: "CW", name: "Centroeste" },
+    { short: "SE", name: "Sudeste" },
+    { short: "S", name: "Sul" },
+  ];
+  
+  const cities = [
+    { state: 'AM', name: 'Manaus', region: 'N'},
+    { state: 'PA', name: 'Belém', region: 'N'},
+    { state: 'TO', name: 'Porto Nacional', region: 'N'},
+    { state: 'MG', name: 'Nepomuceno', region: 'SE'},
+    { state: 'BA', name: 'Cachoeira', region: 'NE'},
+    { state: 'PR', name: 'Curitiba', region: 'S'},
+    { state: 'SP', name: 'Hortolândia', region: 'SE'},
+    { state: 'RN', name: 'Touros', region: 'NE'},
+    { state: 'CE', name: 'Jericoacoara', region: 'NE'},
+    { state: 'TO', name: 'Três Pedras', region: 'N'},
+    { state: 'MG', name: 'João Pinheiro', region: 'SE'},
+    { state: 'MT', name: 'Cuiabá', region: 'CW'},
+  ];
