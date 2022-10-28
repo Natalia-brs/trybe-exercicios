e28c5e162988b6b054029f2b2a169c03fc503b06
natalia <ntla.brs@gmail.com>
2022-10-28 15:52:22 -0300

    Para fixar array destructuring

---
+++ fundamentos/seção-8-high-order-functions-do-javascript-es6/dia-5-spread-operator-parametro-rest-destructuring/Array-destructuring.js
@@ -0,0 +1,32 @@
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+// saudacoes[1](saudacoes[0]); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [greeting] = saudacoes;
+console.log(greeting);
+
+//--------------- A seguir, temos alguns valores que estão descritos em variáveis incoerentes, ---//
+//---------------- através da desestruturação de arrays, corrija os valores das variáveis. ---//
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+// console.log(comida, animal, bebida); // arroz gato água
+
+[comida, animal, bebida] = [bebida, comida, animal];
+ console.log(comida, bebida, animal);
+
+//-----------------------------------------------------------------------------------------------------//
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+[,,, ...numerosPares] = numerosPares
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+//------------------------------------------------------------------------------------------------------------//
