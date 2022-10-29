274e08337d86cce12596da902a8bdfe0aa352717
natalia <ntla.brs@gmail.com>
2022-10-28 21:29:12 -0300

    Exercicio 3

--- fundamentos/seção-8-high-order-functions-do-javascript-es6/dia-5-spread-operator-parametro-rest-destructuring/exercise1.js
+++ fundamentos/seção-8-high-order-functions-do-javascript-es6/dia-5-spread-operator-parametro-rest-destructuring/exercise1.js
@@ -23,7 +23,7 @@ console.log(sum(4,5,6))
 
 //----------------------------------------------------------------------------------------//
 
-//-------------------------
+//------------------------- Modifique o parametro da função PersonLikes --------//
 const alex = {
     name: 'Alex',
     age: 26,
@@ -37,12 +37,12 @@ const alex = {
     likes: ['hiking', 'scuba diving', 'taking pictures'],
     nationality: 'Icelandic',
   };
-  // name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
-  
+
   // complete a assinatura da função abaixo
-  const personLikes = () => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
-  // <nome> tem <anos de idade> e gosta de <gostos da pessoa>
+  const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
   
   // Retornos esperados:
   console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
   console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+  //------------------------------------------------------------------------------------------//
