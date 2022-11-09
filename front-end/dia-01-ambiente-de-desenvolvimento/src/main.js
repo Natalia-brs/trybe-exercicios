21f87377c780057acc925ecd9cd3b8fc18f41516
natalia <ntla.brs@gmail.com>
2022-11-08 21:36:48 -0300

    agora vai

--- front-end/dia-01-ambiente-de-desenvolvimento/src/main.js
+++ front-end/dia-01-ambiente-de-desenvolvimento/src/main.js
@@ -1,5 +1,6 @@
-import { nanoid } from "nanoid"
-import './style.css'
+import { nanoid } from "nanoid";
+import copy from 'clipboard-copy';
+import './style.css';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
@@ -7,4 +8,9 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', async (event) => {
+  await copy(event.target.innerHTML);
+  alert('Senha copiada!');
 });
